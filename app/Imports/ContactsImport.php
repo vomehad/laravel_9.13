45f67edb48aa242a78d908103546afe34d610ee2
d.mozhaev <d.mozhaev@digital-sector.ru>
2022-09-06 13:57:36 +0300

    refactor imports

--- app/Imports/ContactsImport.php
+++ app/Imports/ContactsImport.php
@@ -3,7 +3,6 @@
 namespace App\Imports;
 
 use App\Models\Contact;
-use Illuminate\Database\Eloquent\Model;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 
